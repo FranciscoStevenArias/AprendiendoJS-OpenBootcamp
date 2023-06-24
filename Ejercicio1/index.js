e93e16406b82426dad5fe56edf708e103038e950
FranciscoStevenArias <114457026+FranciscoStevenArias@users.noreply.github.com>
2023-06-24 16:30:29 -0500

    Add files via upload

---
+++ Ejercicio1/index.js
@@ -0,0 +1,11 @@
+
+/*
+//este es el mensaje de bienvenida
+console.log("esta es la puerta de entrada al proyecto")
+//esta es la parte intermedia del proyecto
+//en esta realizamos los chequeos nesesarios
+//por ultimo vamos a la ultima linea de saludo
+//este es el mensaje de despedida
+console.log("Adios")
+*/
+console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo")
